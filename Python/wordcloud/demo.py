ad48add28045cc7aa41ade158ab25a8e72d5d2d5
john_nine <mr.s.szw@gmail.com>
2018-10-17 15:13:09 +0800

    fix

--- Python/wordcloud/demo.py
+++
@@ -1,8 +0,0 @@
-import re
-import xlwt
-import time
-import requests
-import pandas as pd
-from retrying import retry
-from concurrent.futures import ThreadPoolExecutor
-import matplotlib
--- jest/functions.js
+++ jest/functions.js
@@ -1,3 +1,3 @@
 export default {
-  add: (num1, num2) => num1 + num2,
+    add: (num1, num2) => num1 + num2,
 }
